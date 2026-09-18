7936cd655e777e7b5cb9284874c707b9d7028336
harekuto <hillerjojojo@gmail.com>
2026-09-19 04:46:16 +0500

    Set final SMG Android v1.2 metadata

--- scripts/smg_package.ps1
+++ scripts/smg_package.ps1
@@ -3,7 +3,8 @@ Set-StrictMode -Version Latest
 $env:PYTHONUTF8 = '1'
 $env:PACKAGE_NAME = 'com.harekuto.supermonstersgirls'
 $env:APP_NAME = "Super Monsters'n Girls"
-$env:VERSION_NAME = '2.0.2-android-v1'
+$env:VERSION_NAME = '2.0.2-android-v1.2'
+$env:VERSION_CODE = '102'
 
 if (!(Test-Path $env:TEMPLATE_APK)) { throw 'Android runner template missing' }
 if (!(Test-Path $env:MOBILE_WIN)) { throw 'MOBILE_WIN missing' }
