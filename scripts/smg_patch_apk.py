08074c959cc0613910ee37469649181103e9ab2f
harekuto <hillerjojojo@gmail.com>
2026-09-19 16:44:49 +0500

    Update APK metadata defaults for v2

--- scripts/smg_patch_apk.py
+++ scripts/smg_patch_apk.py
@@ -14,8 +14,8 @@ if not manifest.exists():
 
 package_name = os.environ.get("PACKAGE_NAME", "com.harekuto.supermonstersgirls")
 app_name = os.environ.get("APP_NAME", "Super Monsters'n Girls")
-version_name = os.environ.get("VERSION_NAME", "2.0.2-android-v1.2")
-version_code = os.environ.get("VERSION_CODE", "102")
+version_name = os.environ.get("VERSION_NAME", "2.0.2-android-v2")
+version_code = os.environ.get("VERSION_CODE", "200")
 
 s = manifest.read_text(encoding="utf-8")
 m = re.search(r'\bpackage\s*=\s*["\']([^"\']+)["\']', s)
