60e69fe37c1c262aa74174dfe688e308f1291e3f
harekuto <hillerjojojo@gmail.com>
2026-09-21 00:07:52 +0500

    Bump Happy Heart Panic Android to v3

--- happyheart/package.ps1
+++ happyheart/package.ps1
@@ -1,8 +1,8 @@
 $ErrorActionPreference='Stop'
 Set-StrictMode -Version Latest
 $env:HHP_PACKAGE='com.harekuto.happyheartpanic'
-$env:HHP_VERSION='2025-android-v2'
-$env:HHP_VCODE='200'
+$env:HHP_VERSION='2025-android-v3'
+$env:HHP_VCODE='300'
 
 $apktool=Get-ChildItem work/mobiler -Recurse -Filter 'apktool_3.0.3.jar' | Select-Object -First 1
 if(!$apktool){ throw 'apktool missing' }
