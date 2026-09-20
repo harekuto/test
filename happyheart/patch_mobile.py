eaec82f17e3c309727116c2823079437a335deef
harekuto <hillerjojojo@gmail.com>
2026-09-21 00:52:56 +0500

    Make HHP OK button emit Enter consistently

--- happyheart/patch_mobile.py
+++ happyheart/patch_mobile.py
@@ -71,7 +71,7 @@ s=s.replace('virtual_key_settings = virtual_key_add(-50, 5, 38, 50, 92);','virtu
 anchor='virtual_key_c = virtual_key_add((global.dual_controls == 1) ? cx2 : cx, (global.dual_controls == 1) ? cy2 : cy, 27 * button_scale, 29 * button_scale, 67);'
 custom=r'''
     // Core gameplay and menu actions from the game's real input map.
-    virtual_key_hhp_interact = virtual_key_add(hhp_ok_x, hhp_ok_y, hhp_ok_w, hhp_ok_h, 69); // E / OK
+    virtual_key_hhp_interact = virtual_key_add(hhp_ok_x, hhp_ok_y, hhp_ok_w, hhp_ok_h, 13); // Enter: OK in menus, alternate interact in gameplay
     virtual_key_hhp_dash = virtual_key_add(hhp_dash_x, hhp_dash_y, hhp_dash_w, hhp_dash_h, 32); // Space
     virtual_key_hhp_item1 = virtual_key_add(hhp_item1_x, hhp_item1_y, hhp_item1_w, hhp_item1_h, 65); // A
     virtual_key_hhp_item2 = virtual_key_add(hhp_item2_x, hhp_item2_y, hhp_item2_w, hhp_item2_h, 83); // S
@@ -140,7 +140,7 @@ draw_rectangle(hhp_lock_x, hhp_lock_y, hhp_lock_x + hhp_lock_w, hhp_lock_y + hhp
 
 draw_set_alpha(0.72 * image_alpha);
 draw_set_color(c_white);
-draw_text_transformed(hhp_ok_x + hhp_ok_w * 0.5, hhp_ok_y + hhp_ok_h * 0.5, "E / OK", 0.72, 0.72, 0);
+draw_text_transformed(hhp_ok_x + hhp_ok_w * 0.5, hhp_ok_y + hhp_ok_h * 0.5, "OK / USE", 0.72, 0.72, 0);
 draw_text_transformed(hhp_dash_x + hhp_dash_w * 0.5, hhp_dash_y + hhp_dash_h * 0.5, "DASH", 0.70, 0.70, 0);
 draw_text_transformed(hhp_item1_x + hhp_item1_w * 0.5, hhp_item1_y + hhp_item1_h * 0.5, "ITEM 1", 0.60, 0.60, 0);
 draw_text_transformed(hhp_item2_x + hhp_item2_w * 0.5, hhp_item2_y + hhp_item2_h * 0.5, "ITEM 2", 0.60, 0.60, 0);
